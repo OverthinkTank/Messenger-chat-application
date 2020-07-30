a6cbb75432d4fc50dc3f7d59d4578d673c88f61f
OverthinkTank <patelvivekr1152@gmail.com>
2020-07-30 12:21:04 -0700

    Configured firebase

--- src/App.js
+++ src/App.js
@@ -1,63 +1,88 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
-import { Button , FormControl, Input, InputLabel} from '@material-ui/core';
+import { FormControl, Input} from '@material-ui/core';
 import Message from './Message';
+import { db } from './firebase';
+import firebase from 'firebase';
+import FlipMove from 'react-flip-move';
+import SendIcon from '@material-ui/icons/Send';
+import IconButton from '@material-ui/core/IconButton';
+
 function App() {
   //Convention for STATE
   const [input, setInput] = useState('');
-  const [messages,setMessages] = useState([
-    {username: 'Joey' , text: "How you doin'?"},
-    {username: 'Chandler', text: 'I am chandler. I am Funny'},
-]);
+  const [messages,setMessages] = useState([]);
   const [username,setUsername] = useState('');
   
   //useState --> variable in REACT
   //useEffect --> run this code on condition in REACT
+
+  useEffect(()=>{
+    /*-----runs only once when page is loaded bcox at the end in syntax is empty array------*/
+    db.collection('messages')
+    .orderBy('timestamp' , 'desc')
+    .onSnapshot(snapshot => {
+    setMessages(snapshot.docs.map(doc => ({id : doc.id, message: doc.data()})))
+    });
+  }, [] )
+  
+  // submitHandler(e) {
+  //   e.preventDefault();
+  // }
+
   useEffect(()=>{
     /*run condition*/
     /*if it's blank inside[], this code runs once when app component loads*/
     /*if we have variable like input, it will run every single time it see the input */
-    // const name = prop('Please Enter your name'); <--older way
-
-    setUsername(prompt('please enter your name here'));
+    /* const name = prop('Please Enter your name'); <--older way */
+    setUsername(prompt('Please enter your name here'));
   }, [] )
 
-  // console.log(input);
-  // console.log(messages);
-  // console.log(username);
-
   // Core logic of Sending a message
-  const sendMessage = (event)=>{
-      
+  const sendMessage = event => {
+    
+    event.preventDefault();
       /* To prevent page from refreshing we are using event.preventDefault() as we are using <form>(it's default property is to refresh page onclick of SUBMIT button) */
-      event.preventDefault();
+      
       /*...messages used to append input to current message-->spreadout current messages*/
-      setMessages([
-        ...messages, { username:username , text:input }
-      ]);
+      db.collection('messages').add({
+        message: input,
+        username : username,
+        timestamp : firebase.firestore.FieldValue.serverTimestamp()
+      })
+      
+      // setMessages([
+      //   ...messages, { username:username , message:input }
+      // ]);
       setInput('');
   }
 
   return (
     <div className="App">
+      <img src = "https://upload.wikimedia.org/wikipedia/commons/thumb/8/83/Facebook_Messenger_4_Logo.svg/100px-Facebook_Messenger_4_Logo.svg.png?h=100&w=100" />
       <h1>Welcome to fb-messenger</h1>
       <h2>Welcome {username}</h2>
-        <FormControl>
-          <InputLabel htmlFor="my-input">Enter a message</InputLabel>
-          <Input value = {input} onChange={event=>setInput(event.target.value)}/>    
-          <Button disabled = {!input} variant = "contained" color = "primary" onClick={sendMessage}>SEND</Button>
-        </FormControl>
-        
+        <div className = "form_container">
+        <form className = "app_form" onSubmit = {sendMessage}>
+          <FormControl className = "app_formControl" >
+            <Input className = "app_input" placeholder = "Enter a message..." value = {input} onChange={event=>setInput(event.target.value)}/>    
+            <IconButton className = "app_iconButton" disabled = {!input} variant = "contained" color = "primary" onClick={sendMessage}>
+              <SendIcon />
+            </IconButton>
+          </FormControl>
+        </form>
+      </div>
+
+      <FlipMove>
         {
-          messages.map(message => (
+          messages.map(({id,message}) => (
           /*We created Message.js and using it here as prop*/
-          <Message username = {username} message = {message} />
+          <Message key={id} username = {username} message = {message} />
           ))
         }
-
-
-
-    </div>
+      </FlipMove>
+        
+      </div>
   );
 }
 
--- src/Message.js
+++ src/Message.js
@@ -1,21 +1,24 @@
-import React from 'react'
+import React, { forwardRef } from 'react';
 import { Typography, CardContent, Card } from '@material-ui/core'
 import './Message.css';
-function Message({message , username}) {
+
+//Message is a function
+const Message = forwardRef(({message , username},ref) => {
     const isUser = username === message.username;
     // console.log(isUser);
     return (
-        <div  className = {`message_card ${isUser && 'message_sender'}`} >
+        <div  ref = {ref} className = {`message ${isUser && 'message_sender'}`} >
             <Card className = {isUser ? "message_sender" : "message_receiver"}>
                 
                 <CardContent>
                     <Typography variant = "h5" component = "h2">
-                        {message.username}:  {message.text}                
+                        {!isUser && `${message.username || 'Anonymous User'}: `} {message.message}                
                     </Typography>
-                </CardContent>   
+                </CardContent> 
+
             </Card>  
         </div>
     )
-}
+})
 
 export default Message
